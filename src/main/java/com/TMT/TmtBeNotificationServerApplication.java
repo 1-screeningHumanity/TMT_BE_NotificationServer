0415715b4aa3f95a550c3e7f1e905c7544c53f29
ddohyeong <dohyeong7026@gmail.com>
2024-06-03 15:47:13 +0900

    repo 생성

---
+++ src/main/java/com/TMT/TmtBeNotificationServerApplication.java
@@ -0,0 +1,13 @@
+package com.TMT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmtBeNotificationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmtBeNotificationServerApplication.class, args);
+	}
+
+}
