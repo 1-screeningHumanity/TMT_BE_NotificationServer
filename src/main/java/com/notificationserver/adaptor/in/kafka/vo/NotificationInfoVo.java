ba28144a306ab9a2d5c508511a821f9e5ac84c11
ddohyeong <dohyeong7026@gmail.com>
2024-06-09 23:02:21 +0900

    Feat: kafka로 다른 서비스로 부터 알림 내용 메시징 리스너 기능 구현

---
+++ src/main/java/com/notificationserver/adaptor/in/kafka/vo/NotificationInfoVo.java
@@ -0,0 +1,10 @@
+package com.notificationserver.adaptor.in.kafka.vo;
+
+import lombok.Getter;
+
+@Getter
+public class NotificationInfoVo {
+	private String title;
+	private String body;
+	private String uuid;
+}
